27ce6ea7eb5bfcfff1bd514af269ecaa94d2300f
Raghav539 <raghavgaurpandit@gmail.com>
2025-08-15 12:50:27 +0530

    Crud Api Done

--- noteapp/urls.py
+++ noteapp/urls.py
@@ -4,5 +4,10 @@ from .import views
 
 urlpatterns =[
     path("notes/",views.notes,name="notes"),
-    path("notes/<slug:slug>",views.note_detail,name="note-detail")
+    path("notes/<slug:slug>/",views.note_detail,name="note-detail")
 ]
+
+# endpoints:
+# GET_ALL_NOTES_and_CREATE_NEW_NOTE = "127.0.0.1:8008/notes/"
+# GET_SPECIFIC_NOTE = "127.0.0.1:8008/notes/note-slug"
+# SEARCH_NOTES = "127.0.0.1:8008/notes-search/?search=meeting"
