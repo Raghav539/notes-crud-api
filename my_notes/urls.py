23048453aa68cd527166946f7f6c68ee2f161e35
Raghav539 <raghavgaurpandit@gmail.com>
2025-08-18 11:37:26 +0530

    Make User App For Login and Register

--- my_notes/urls.py
+++ my_notes/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("noteapp.urls")),
     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
-    path('api/toekn/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
